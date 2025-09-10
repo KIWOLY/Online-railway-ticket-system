37631c128458ce72d532dfc62ce2cbb3b7358dbe
Innocent john kiwoly <166690829+KIWOLY@users.noreply.github.com>
2025-09-10 12:54:24 +0300

    Set DEBUG to False in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p@rkov+t)epr9l!e&t5ns+1njm@(mqjfo17f0ifw62k$qd7g76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
